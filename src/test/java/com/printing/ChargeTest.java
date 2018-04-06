ecbdedc81a4198f0a6566b82b2173c6935315b8f
wanching97 <wanching_97@hotmail.com>
2018-04-06 11:34:10 +0800

    added ChargeTest class

--- src/test/java/com/printing/ChargeTest.java
+++ src/test/java/com/printing/ChargeTest.java
@@ -1,13 +1,33 @@
 package com.printing;
 
-import com.printing.domain.OrderList;
+import com.printing.domain.Charge;
+import com.printing.domain.Order;
+import junitparams.JUnitParamsRunner;
 import junitparams.Parameters;
-import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static junit.framework.TestCase.assertEquals;
 
+@RunWith(JUnitParamsRunner.class)
 public class ChargeTest {
 
+    @Test
+    @Parameters(method = "getParamsForTestReceiveOrder")
+    public void testReceiveOrders(int quantity) {
+        Charge charge = new Charge();
+        for(int i = 0; i < quantity; i++) {
+            charge.receiveOrder(new Order(true, true));
+        }
+        assertEquals(quantity, charge.getQuantity());
+    }
+
+    public Object[] getParamsForTestReceiveOrder() {
+        return new Object[] {
+                new Object[]{0},
+                new Object[]{2},
+                new Object[]{5}
+        };
+    }
 
 }
