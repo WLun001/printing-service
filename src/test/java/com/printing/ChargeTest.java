a39e4eb9633c7b449bbc167b1b42a75ff36ae546
wanching97 <wanching_97@hotmail.com>
2018-04-06 11:16:07 +0800

    added DecisionTableTest class

--- src/test/java/com/printing/ChargeTest.java
+++ src/test/java/com/printing/ChargeTest.java
@@ -1,4 +1,13 @@
 package com.printing;
 
+import com.printing.domain.OrderList;
+import junitparams.Parameters;
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class ChargeTest {
+
+
 }
