20e8b0d3e981af0f364ece027bfcaecb2313c690
Wei Lun <weilun_95@hotmail.com>
2018-04-06 14:14:17 +0800

    mock test for get total number of order

--- src/test/java/com/printing/AppControllerTest.java
+++ src/test/java/com/printing/AppControllerTest.java
@@ -2,13 +2,19 @@ package com.printing;
 
 import com.printing.domain.AppController;
 import com.printing.domain.Charge;
+import com.printing.domain.Order;
 import com.printing.domain.OrderList;
+import junitparams.Parameters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.internal.matchers.Or;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -25,6 +31,16 @@ public class AppControllerTest {
         ac.addOrder(true, true);
         ac.addOrder(true, true);
         verify(od, times(2)).addOrder(true, true);
+    }
+
+    @Test
+    public void testGetNumberOfOrders(){
+        for (int i = 0; i < 10; i ++){
+            ac.addOrder(true, true);
+        }
+        ac.getNumberOfOrders();
+        verify(od, times(1)).getNumberOfOrders();
+
     }
 
     @Test
